e095f9428d7879493191e7d1c664788c8b4d2b0f
adithyaa-rg <adithyaa2003@gmail.com>
2023-07-07 12:46:31 +0530

    Completed

--- Etch-a-Sketch.js
+++ Etch-a-Sketch.js
@@ -2,6 +2,7 @@ function createGrid(dimension){
     let sketch_grid = document.querySelector('.sketch-grid');
     for (let i = 0; i < dimension; i++){
         let div_row = document.createElement('div');
+        div_row.setAttribute('id',`box-${i}`);
         sketch_grid.appendChild(div_row);
         for (let j = 0; j < dimension; j++){
             let div_column = document.createElement('div');
@@ -14,12 +15,20 @@ function createGrid(dimension){
     }
 }
 
+function deleteGrid(dimension){
+    console.log("hi");
+    let sketch_grid = document.querySelector('.sketch-grid');
+    sketch_grid.innerHTML = '';
+}
+
 function OverBoxComplete(e){
+
     let id_box = e.target.id;
     let colors = ["rgb(238, 238, 238)", "rgb(204, 204, 204)", "rgb(170, 170, 170)", "rgb(153, 153, 153)", "rgb(119, 119, 119)", "rgb(85, 85, 85)", "rgb(51, 51, 51)", "rgb(34, 34, 34)", "rgb(17, 17, 17)", "#000000"]
     if (id_box && mouseIsDown){
         let elementID = document.getElementById(id_box);
         let currentColor = elementID.style.background;
+        console.log(colourMode);
         if (colourMode === 'Grayscale'){
             if (currentColor == 'rgb(225, 250, 255)'){
                 elementID.style.background = colors[0];
@@ -39,30 +48,44 @@ function OverBoxComplete(e){
         }
     }
 }
-function changeSlideValue(){
+function changeSlideValue(initialValue){
     sliderValue = slider.value;
     let sliderDiv = document.getElementById('slideValue');
-    console.log(sliderDiv);
     sliderDiv.textContent = sliderValue;
-    var colourMode = colourElement.value;
-    
+    colourMode = colourElement.value;
+    if (sliderValue != initialValue)
+    {
+        deleteGrid(parseInt(initialValue));
+        createGrid(parseInt(sliderValue));
+    }
 }
 
+function resetOption(){
+    sliderValue = slider.value;
+    deleteGrid(parseInt(sliderValue));
+    createGrid(parseInt(sliderValue));
+}
 var slider = document.getElementById("myRange");
 var colourElement = document.getElementById("colourSelect");
 var mouseIsDown = false;
 var sliderValue = slider.value;
 var colourMode = colourElement.value;
 
-changeSlideValue()
+changeSlideValue(sliderValue)
 game();
 function game(){
     createGrid (parseInt(sliderValue));
-    addEventListener('mousedown', (e) => {mouseIsDown = true;});
+    addEventListener('mouseover', OverBoxComplete);
+    addEventListener('mousedown', (e) => {
+        mouseIsDown = true;
+    });
+
     addEventListener('mouseup', (e) => {
         mouseIsDown = false;
-        changeSlideValue();
+        changeSlideValue(sliderValue);
     });
-    addEventListener('mouseover', OverBoxComplete);
+    
+    let resetButton = document.querySelector('.gameplay');
+    resetButton.addEventListener('click', resetOption);
 }
 
